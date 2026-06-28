ffc864b3bfea194f5780233a158c4ad60fa88f6a
engfabiodesalvi <engfabiodesalvi@gmail.com>
2026-06-28 13:53:02 -0300

    feat: create CacheInterface for decoupling file caching
    Introduce contract to enable file-based caching mechanism.
    
    Decouple caching logic to allow future storage implementations without breaking library compatibility.

---
+++ src/Contracts/CacheInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+// Desacoplamento para uso de cache em arquivos e possibilitar futura implementação em:
+// - Redis
+// - Memcached
+// - APCu
+// Sem alterar a biblioteca
+
+declare(strict_types=1);
+
+namespace Engfabiodesalvi\BuscaCepPhp\Contracts;
+
+interface CacheInterface
+{
+    public function has(string $key): bool;
+
+    public function get(string $key): mixed;
+
+    public function set(
+        string $key,
+        mixed $value,
+        int $ttl = 3600
+    ): void;
+
+    public function delete(string $key): void;
+
+    public function clear() : void;
+}
