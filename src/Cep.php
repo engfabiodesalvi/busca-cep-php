8b988684003c13cb7eb3a8797f95fe561f310c90
engfabiodesalvi <engfabiodesalvi@gmail.com>
2026-06-28 00:38:44 -0300

    fix(namespace): resolve incorrect namespaces

--- src/Cep.php
+++ src/Cep.php
@@ -3,7 +3,7 @@
 // Força declaração dos tipos das variáveis, evitando erros inesperados de conversão automática de tipos de variáveis.
 declare(strict_types=1);
 
-namespace Engfabiodesalvi\BuscaCepPhp\CepSearch;
+namespace Engfabiodesalvi\BuscaCepPhp;
 
 // Classe que representa a principal porta de entrada da bibliteca.
 // Utilizar "final" evita geranças indevidas e força extençao por composição, prática comum em biblitecas modernas
