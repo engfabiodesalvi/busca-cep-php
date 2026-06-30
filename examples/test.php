a77e5f4c86829fc39cdda3fb2a13f11e38833ec8
engfabiodesalvi <engfabiodesalvi@gmail.com>
2026-06-29 23:53:02 -0300

    chore(config): update package stability and example script
    Set minimum-stability to dev in composer.json
    
    Update examples/test.php to test new OpenCEP and BrasilAPI providers

--- examples/test.php
+++ examples/test.php
@@ -5,7 +5,59 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Engfabiodesalvi\BuscaCepPhp\CepSearch;
+use Engfabiodesalvi\BuscaCepPhp\Domain\ValueObject\Cep;
+use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Http\HttpClient;
+use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Normalizers\BrasilApiNormalizer;
+use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Normalizers\OpenCepNormalizer;
+use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Normalizers\ViaCepNormalizer;
+use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Providers\BrasilApiProvider;
+use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Providers\OpenCepProvider;
+use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Providers\ViaCepProvider;
 
 $cep = new CepSearch();
 
 var_dump($cep);
+
+// Via Cep
+$viaCepProvider = new ViaCepProvider(
+    new HttpClient(),
+    new ViaCepNormalizer
+);
+
+var_dump($viaCepProvider);
+
+
+$addressViaCep = $viaCepProvider->search(
+    new Cep('01001000')
+);
+
+echo $addressViaCep->__toString() . "\n";
+
+
+// Brasil API
+$brasilApiProvider = new BrasilApiProvider(
+    new HttpClient(),
+    new BrasilApiNormalizer
+);
+
+var_dump($brasilApiProvider);
+
+$addressBrasilApi = $brasilApiProvider->search(
+    new Cep('01001000')
+);
+
+echo $addressBrasilApi->__toString() . "\n";
+
+// Open CEP
+$openCepProvider = new openCepProvider(
+    new HttpClient(),
+    new OpenCepNormalizer
+);
+
+var_dump($openCepProvider);
+
+$addressOpenCep = $openCepProvider->search(
+    new Cep('01001000')
+);
+
+echo $addressOpenCep->__toString() . "\n";
