e02ee1408baedb24f38c6e7a10bfb56ed5b1f7ba
engfabiodesalvi <engfabiodesalvi@gmail.com>
2026-07-08 14:07:09 -0300

    test: implement unit test for AwesomeApiNormalizer using DataProvider
    Add testShouldNormalizeAwesomeApiDataProviderResponse to validate API
    response normalization. Implement cepTestData as a static data provider
    using modern PHP 8 PHPUnit attributes.

--- tests/Infrastructure/Normalizers/AwesomeApiNormalizerTest.php
+++ tests/Infrastructure/Normalizers/AwesomeApiNormalizerTest.php
@@ -6,6 +6,7 @@ namespace Engfabiodesalvi\BuscaCepPhp\Tests\Infrastructure\Normalizers;
 
 use Engfabiodesalvi\BuscaCepPhp\Domain\DTO\Address;
 use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Normalizers\AwesomeApiNormalizer;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 final class AwesomeApiNormalizerTest extends TestCase
@@ -90,6 +91,101 @@ final class AwesomeApiNormalizerTest extends TestCase
             $address->provider()->label()
         );
     }
+
+    // Teste utilizando dataprovider 
+    // Anotação abaixo foi descontinuada a partir de versão 10 do PHP!!!
+    /**
+     *  @dataProvider cepTestData
+     */
+    // Utilize esta anotação:
+    #[DataProvider('cepTestData')]
+    public function testShouldNormalizeAwesomeApiDataProviderResponse(array $data): void
+    {
+        $normalizer = new AwesomeApiNormalizer();
+
+        $address = $normalizer->normalize($data);
+
+        $this->assertInstanceOf(
+            Address::class,
+            $address
+        );
+
+        $this->assertSame(
+            $data['cep'], // '01001000',
+            $address->cep()
+        );
+
+        $this->assertSame(
+            $data['address'], // 'Praça da Sé',
+            $address->street()
+        );
+
+        $this->assertSame(
+            '',
+            $address->complement()
+        );
+
+        $this->assertSame(
+            $data['district'], // 'Sé',
+            $address->district()
+        );
+
+        $this->assertSame(
+            $data['city'], // 'São Paulo',
+            $address->city()
+        );
+
+        $this->assertSame(
+            $data['state'], // 'SP',
+            $address->state()
+        );
+
+        $this->assertSame(
+            $data['city_ibge'], // '3550308',
+            $address->ibge()
+        );
+
+        $this->assertSame(
+            '',
+            $address->gia()
+        );
+
+        $this->assertSame(
+            $data['ddd'], // '11',
+            $address->ddd()
+        );
+
+        $this->assertSame(
+            '',
+            $address->siafi()
+        );
+
+        $this->assertSame(
+            $data['provider'], // 'AwesomeAPI',
+            $address->provider()->label()
+        );
+    }
+    
+    public static function cepTestData(): array {
+        return [
+            "Address Praça da Sé" => [  
+                "data" => [
+                    'cep' => '01001000',
+                    'address' => 'Praça da Sé',
+                    //'complement' => '',
+                    'district' => 'Sé',
+                    'city' => 'São Paulo',
+                    'state' => 'SP',
+                    'city_ibge' => '3550308',
+                    //'gia' => '',
+                    'ddd' => '11',
+                    //'siafi' => '',
+                    'provider' => 'AwesomeAPI', 
+                ]                             
+            ]    
+        ];
+    }
+
 }
 
 // Awesome API    : Praça da Sé, Sé - São Paulo/SP
