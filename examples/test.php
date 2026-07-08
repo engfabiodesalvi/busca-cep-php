8738ad91dedc2c3e8ad366b2567f106205f3a617
engfabiodesalvi <engfabiodesalvi@gmail.com>
2026-07-07 23:42:41 -0300

    test: update test script with token config, error handling, and formatting
    Add Cep Aberto token configuration and wrap execution in try/catch blocks for library exceptions.
    
    Implement printAddress() helper function to format and output Address attributes.

--- examples/test.php
+++ examples/test.php
@@ -6,6 +6,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Engfabiodesalvi\BuscaCepPhp\CepSearch;
 use Engfabiodesalvi\BuscaCepPhp\Collections\ProviderCollection;
+use Engfabiodesalvi\BuscaCepPhp\Config\Config;
+use Engfabiodesalvi\BuscaCepPhp\Domain\DTO\Address;
+use Engfabiodesalvi\BuscaCepPhp\Domain\Exceptions\CepException;
 use Engfabiodesalvi\BuscaCepPhp\Domain\ValueObject\Cep;
 use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Http\HttpClient;
 use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Normalizers\AwesomeApiNormalizer;
@@ -19,76 +22,153 @@ use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Providers\CepAbertoProvider;
 use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Providers\OpenCepProvider;
 use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Providers\ViaCepProvider;
 
+function printAddress(Address $address): void
+{
+    echo 'CEP: ' . $address->cep() . PHP_EOL;
+    echo 'Logradouro: ' . $address->street() . PHP_EOL;
+    echo 'Complemento: ' . $address->complement() . PHP_EOL;
+    echo 'Bairro: ' . $address->district() . PHP_EOL;
+    echo 'Cidade: ' . $address->city() . PHP_EOL;
+    echo 'UF: ' . $address->state() . PHP_EOL;
+    echo 'IBGE: ' . $address->ibge() . PHP_EOL;
+    echo 'GIA: ' . $address->gia() . PHP_EOL;
+    echo 'DDD: ' . $address->ddd() . PHP_EOL;
+    echo 'SIAFI: ' . $address->siafi() . PHP_EOL;
+    echo 'Provider: ' . $address->provider()->label() .  PHP_EOL;    
+}
+
 $cep = new CepSearch();
 
 // var_dump($cep);
 
-// Via Cep
-$viaCepProvider = new ViaCepProvider(
-    new HttpClient()
-);
+//-------------------
 
-// var_dump($viaCepProvider);
+try{
 
+    // Via Cep
+    $viaCepProvider = new ViaCepProvider(
+        new HttpClient()
+    );
 
-$addressViaCep = $viaCepProvider->search(
-    new Cep('01001000')
-);
-
-// echo $addressViaCep->toJson() . "\n";
-echo sprintf("%-15s", "Via Cep") . ": " . $addressViaCep->__toString() . "\n";
+    // var_dump($viaCepProvider);
 
 
-// Brasil API
-$brasilApiProvider = new BrasilApiProvider(
-    new HttpClient()
-);
+    $addressViaCep = $viaCepProvider->search(
+        new Cep('01001000')
+    );
 
-// var_dump($brasilApiProvider);
+    // echo $addressViaCep->toJson() . "\n";
+    echo sprintf("%-15s", "Via Cep") . ": " . $addressViaCep->__toString() . "\n";
 
-$addressBrasilApi = $brasilApiProvider->search(
-    new Cep('01001000')
-);
+    printAddress($addressViaCep);
 
-echo sprintf("%-15s", "Brasil API") . ": " . $addressBrasilApi->__toString() . "\n";
+} catch (CepException $e) {
 
-// Open CEP
-$openCepProvider = new OpenCepProvider(
-    new HttpClient()
-);
+    echo 'Erro: ' . $e->getMessage() . PHP_EOL;
+}
 
-// var_dump($openCepProvider);
+//-------------------
 
-$addressOpenCep = $openCepProvider->search(
-    new Cep('01001000')
-);
+try {
 
-echo sprintf("%-15s", "Open CEP") . ": " . $addressOpenCep->__toString() . "\n";
+    // Brasil API
+    $brasilApiProvider = new BrasilApiProvider(
+        new HttpClient()
+    );
 
-// Awesome API
+    // var_dump($brasilApiProvider);
 
-$awesomeApiProvider = new AwesomeApiProvider(
-    new HttpClient()
-);
+    $addressBrasilApi = $brasilApiProvider->search(
+        new Cep('01001000')
+    );
 
-// var_dump($awesomeApiProvider);
+    echo sprintf("%-15s", "Brasil API") . ": " . $addressBrasilApi->__toString() . "\n";
 
-$addressAwesomeApi = $awesomeApiProvider->search(
-    new Cep('01001000')
-);
+    printAddress($addressBrasilApi);
 
-echo sprintf("%-15s", "Awesome API") . ": " . $addressAwesomeApi->__toString() . "\n";
+} catch (CepException $e) {
 
-// CEP Aberto
+    echo 'Erro: ' . $e->getMessage() . PHP_EOL;
+}
 
-$cepAbertoProvider = new CepAbertoProvider(
-    new HttpClient()
-);
+//-------------------
 
-// var_dump($cepAbertoProvider);
+try {
 
-$addressCepAberto = $cepAbertoProvider->search(
-    new Cep('01001000')
-);
+    // Open CEP
+    $openCepProvider = new OpenCepProvider(
+        new HttpClient()
+    );
 
-echo sprintf("%-15s", "CEP Aberto") . ": " . $addressCepAberto->__toString() . "\n";
+    // var_dump($openCepProvider);
+
+    $addressOpenCep = $openCepProvider->search(
+        new Cep('01001000')
+    );
+
+    echo sprintf("%-15s", "Open CEP") . ": " . $addressOpenCep->__toString() . "\n";
+
+    printAddress($addressOpenCep);
+
+} catch (CepException $e) {
+
+    echo 'Erro: ' . $e->getMessage() . PHP_EOL;
+}
+
+//-------------------
+
+try {
+
+    // Awesome API
+
+    $awesomeApiProvider = new AwesomeApiProvider(
+        new HttpClient()
+    );
+
+    // var_dump($awesomeApiProvider);
+
+    $addressAwesomeApi = $awesomeApiProvider->search(
+        new Cep('01001000')
+    );
+
+    echo sprintf("%-15s", "Awesome API") . ": " . $addressAwesomeApi->__toString() . "\n";
+
+    printAddress($addressAwesomeApi);
+
+} catch (CepException $e) {
+
+    echo 'Erro: ' . $e->getMessage() . PHP_EOL;
+}
+
+//-------------------    
+
+try {
+
+    // CEP Aberto
+
+    // Configura o token
+    Config::set(
+        'cepaberto_token',
+        '' // your_token_here
+    );    
+
+    $cepAbertoProvider = new CepAbertoProvider(
+        new HttpClient()
+    );
+
+    // var_dump($cepAbertoProvider);
+
+    $addressCepAberto = $cepAbertoProvider->search(
+        new Cep('01001000')
+    );
+
+    echo sprintf("%-15s", "CEP Aberto") . ": " . $addressCepAberto->__toString() . "\n";
+
+    printAddress($addressCepAberto);
+
+} catch (CepException $e) {
+
+    echo 'Erro: ' . $e->getMessage() . PHP_EOL;
+}
+
+//-------------------
