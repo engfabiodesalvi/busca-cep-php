1575f844c7f6de4166e2daecaef9e4c7d216f383
engfabiodesalvi <engfabiodesalvi@gmail.com>
2026-07-08 04:31:48 -0300

    refactor: define structural boundaries and responsibilities for CepSearch Facade
    Establish CepSearch as a pure Facade with only two responsibilities: component instantiation and delegation to CepService.
    
    Explicitly state that this class contains no business logic.

--- src/CepSearch.php
+++ src/CepSearch.php
@@ -16,6 +16,7 @@ use Engfabiodesalvi\BuscaCepPhp\Application\Services\CepService;
 use Engfabiodesalvi\BuscaCepPhp\Domain\DTO\Address;
 use Engfabiodesalvi\BuscaCepPhp\Domain\ValueObject\Cep;
 use Engfabiodesalvi\BuscaCepPhp\Factory\ProviderFactory;
+use Engfabiodesalvi\BuscaCepPhp\Infrastructure\Logging\ConsoleLogger;
 
 // Classe que representa a principal porta de entrada da bibliteca.
 // Utilizar "final" evita mudanças indevidas e força extençao por composição, prática comum em biblitecas modernas
@@ -23,12 +24,15 @@ final class CepSearch
 {
     private CepService $service;
 
-    public function __construct()
-    {
+    public function __construct(
+        // private readonly array $tokens = []
+    ) {
         $factory = new ProviderFactory();
 
         $this->service = new CepService(
-            $factory->create()
+            providers: $factory->create(),
+            // tokens: $this->tokens,
+            // logger: new ConsoleLogger()
         );
     }
 
