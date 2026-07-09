4db29a41ef9b0ba05a7daa46428e5f65685b939b
engfabiodesalvi <engfabiodesalvi@gmail.com>
2026-07-09 00:58:23 -0300

    feat: create a ViaCep class that performs the lookup of zip code information

---
+++ src/WebService/ViaCep.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Engfabiodesalvi\BuscaCepPhp\WebService;
+
+class ViaCep
+{
+    // O CEP será buscado nesta URL
+    private string $url;
+
+    public function __construct() {
+        $this->url = "https://viacep.com.br/ws/";
+    }    
+
+    public function search(string $zipCode): array
+    {    
+        // Remove caracteres indesejados
+        $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
+
+        // Realiza a requisição do CEP desejado com o retorno dos dados no formato json
+        $get = file_get_contents($this->url . $zipCode . "/json");
+
+        // Retorna o resultado
+        return (array) json_decode($get);    
+    }
+
+}
